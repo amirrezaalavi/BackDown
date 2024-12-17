994c0cd628a445ec5aba34eaccc3dc60f9141dcf
amiralavi-acer <amiralavi@localhost.localdomain>
2024-12-17 23:17:18 +0330

    convert to OOP

--- initialize.py
+++ initialize.py
@@ -1,7 +1,6 @@
 import os
-from http import server
 
-def initialize():
+class profile:
 
     client_folder_path = "./"
     server_ip = ""
@@ -9,49 +8,53 @@ def initialize():
     server_remote_folder = ""
     server_username = ""
     server_password = ""
-    temp = ""
-    check = True # For Ensuring every data Enters correctly
+    def __init__(self): # Just forwarded to be able to change in case of adding database
+        self.initialize()
 
-    print("Initialization ...")
-    # Input every temp file with temp variable, then assign it
-    # Starting with client_folder_path
-    while check:
-        temp = input("Enter the Path to the folder on the computer : ")
-        if (os.path.exists(temp) == False):
-            print("Wrong Path, Try Again")
-            continue
-        else:
-            client_folder_path = temp
-            print(f"Local folder changed to {client_folder_path}")
-            temp = ""
-            break
-    # server_ip
-    while check:
-        temp = input("Enter ip address of the server (IPv4 only): ")
-        valid = True
-        temp_ip = temp.split('.')
-        for part in temp_ip :
-            try :
-                int(part)
-            except :
-                valid = False
-                print("Wrong IP address, try again")
-                break
-        if(not valid):
-            break
-        server_ip = temp
-        print(f"IP successfully changed to {server_ip}")
+
+    def initialize(self):
         temp = ""
-    # This part is for data that we can't check
-    while check:
-        temp = input("Enter the name of the shared folder on the Server : ")
-        if(temp == ""):
-            print("String empty")
-            continue
-        server_share = temp
-        server_remote_folder = input("Enter the address of the folder on the Server : ") # This can possibly be empty and its fine
+        check = True # For Ensuring every data Enters correctly, just in case we want to save data to database later
+        print("Initialization ...")
+        # Input every temp file with temp variable, then assign it
+        # Starting with client_folder_path
+        while check:
+            temp = input("Enter the Path to the folder on the computer : ")
+            if (os.path.exists(temp) == False):
+                print("Wrong Path, Try Again")
+                continue
+            else:
+                self.client_folder_path = temp
+                print(f"Local folder changed to {self.client_folder_path}")
+                temp = "" # Just for preventing problems
+                break
+        # server_ip
+        while check:
+            temp = input("Enter ip address of the server (IPv4 only): ")
+            valid = True
+            temp_ip = temp.split('.')
+            for part in temp_ip :
+                try :
+                    int(part)
+                except :
+                    valid = False
+                    print("Wrong IP address, try again")
+                    break
+            if(not valid):
+                break
+            self.server_ip = temp
+            print(f"IP successfully changed to {self.server_ip}")
+            temp = ""
+        # This part is for data that we can't check
+        while check:
+            temp = input("Enter the name of the shared folder on the Server : ")
+            if(temp == ""):
+                print("String empty")
+                continue
+            self.server_share = temp
+            self.server_remote_folder = input("Enter the address of the folder on the Server : ") # This can possibly be empty and its fine
 
-    # In this part we collect optional data
-    server_username = input("Enter Server username (optional) : ")
+        # In this part we collect optional data
+        self.server_username = input("Enter Server username (optional) : ")
 
-    server_password = input("Enter Server password (optional) : ")
+        self.server_password = input("Enter Server password (optional) : ")
